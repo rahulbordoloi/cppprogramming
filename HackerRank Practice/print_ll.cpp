791186e406b1c077d025c4700853eddb0928766b
Rahul Bordoloi <rahulbordoloi24@gmail.com>
2020-08-29 11:04:50 +0530

    Print LL Problem

---
+++ HackerRank Practice/print_ll.cpp
@@ -0,0 +1,119 @@
+/*
+This challenge is part of a MyCodeSchool tutorial track and is accompanied by a video lesson.
+
+If you're new to linked lists, this is a great exercise for learning about them. Given a pointer to the head node of a linked list, print its elements in order, one element per line. If the head pointer is null (indicating the list is empty), don’t print anything.
+
+Input Format
+
+The first line of input contains , the number of elements in the linked list.
+The next  lines contain one element each, which are the elements of the linked list.
+
+Note: Do not read any input from stdin/console. Complete the printLinkedList function in the editor below.
+
+Constraints
+
+, where  is the  element of the linked list.
+Output Format
+
+Print the integer data for each element of the linked list to stdout/console (e.g.: using printf, cout, etc.). There should be one element per line.
+
+Sample Input
+
+2
+16
+13
+Sample Output
+
+16
+13
+Explanation
+
+There are two elements in the linked list. They are represented as 16 -> 13 -> NULL. So, the printLinkedList function should print 16 and 13 each in a new line.
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class SinglyLinkedListNode {
+    public:
+        int data;
+        SinglyLinkedListNode *next;
+
+        SinglyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+        }
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+        SinglyLinkedListNode *tail;
+
+        SinglyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            SinglyLinkedListNode* node = new SinglyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+            }
+
+            this->tail = node;
+        }
+};
+
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+// Complete the printLinkedList function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+void printLinkedList(SinglyLinkedListNode* head) {
+    while(head != NULL){
+      cout << head->data << endl;
+      head = head->next;
+  }
+}
+
+int main()
+{
+    SinglyLinkedList* llist = new SinglyLinkedList();
+
+    int llist_count;
+    cin >> llist_count;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int i = 0; i < llist_count; i++) {
+        int llist_item;
+        cin >> llist_item;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        llist->insert_node(llist_item);
+    }
+
+    printLinkedList(llist->head);
+
+    return 0;
+}
