94e38948ca5f00edabd174038340404294a8c6d6
Rahul Bordoloi <rahulbordoloi24@gmail.com>
2020-09-17 07:49:50 +0530

    To Lower Case

---
+++ Leetcode Practice/to_lower_case.cpp
@@ -0,0 +1,33 @@
+// Link - https://leetcode.com/problems/to-lower-case/
+
+/*
+709. To Lower Case
+
+Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+
+ 
+
+Example 1:
+
+Input: "Hello"
+Output: "hello"
+Example 2:
+
+Input: "here"
+Output: "here"
+Example 3:
+
+Input: "LOVELY"
+Output: "lovely"
+*/
+
+class Solution {
+public:
+    string toLowerCase(string str) {
+        
+        for(char& i : str){
+            i = tolower(i);
+        }
+        return str;
+    }
+};
