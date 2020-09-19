aa2250ddf473c37690086ee6a00ab8b08a31cfdc
Rahul Bordoloi <rahulbordoloi24@gmail.com>
2020-09-19 14:44:57 +0530

    My CPP Template

---
+++ cppTemplate.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+#define fast ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL)
+#define f(i,a,b) for(i=a;i<b;i++)
+#define fi(i,a,b,x) for(i=a;i<b;i=i+x)
+#define fe(i,a,b) for(i=a;i<=b;i++)
+#define fr(i,a,b) for(i=a;i>=b;i--)
+#define endl '\n'
+#define sp '\t'
+#define ll long long int
+#define ff first
+#define ss second
+#define pb push_back
+#define mp make_pair
+#define mod 1000000007
+
+using namespace std;
+
+void print(vector<ll>& a, char sep = ' ')
+{
+    for(auto i : a) { cout<<i<<sep; }
+}
+
+ll mod_opr(ll num)
+{
+    return num % mod;
+}
+
+bool compare(ll x, ll y)
+{
+    return x > y ? true : false;
+}
+
+int main()
+{
+    
+    cout<<"Hello World"<<endl;
+    return 0;
+    
+}
