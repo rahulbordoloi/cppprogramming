52454b4ca04c81ed471b23a9f7b88b47ba907d00
Rahul Bordoloi <rahulbordoloi24@gmail.com>
2020-09-24 07:21:08 +0530

    Squares of a Sorted Array

---
+++ Leetcode Practice/sq_sorted_arr.cpp
@@ -0,0 +1,38 @@
+// Link - https://leetcode.com/problems/squares-of-a-sorted-array/
+
+977. Squares of a Sorted Array
+
+Given an array of integers A sorted in non-decreasing order, return an array of the squares of each number, also in sorted non-decreasing order.
+
+ 
+
+Example 1:
+
+Input: [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+Example 2:
+
+Input: [-7,-3,2,3,11]
+Output: [4,9,9,49,121]
+ 
+
+Note:
+
+1 <= A.length <= 10000
+-10000 <= A[i] <= 10000
+A is sorted in non-decreasing order.
+
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& A) {
+        
+        vector<int> answers;
+        for(auto i : A){
+            answers.push_back(i*i);
+        }
+        sort(answers.begin(), answers.end());
+        
+        return answers;
+        
+    }
+};
