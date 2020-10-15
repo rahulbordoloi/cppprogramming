fe73b2772fba748fda815a66b9ad97c6ab54000c
Rahul Bordoloi <rahulbordoloi24@gmail.com>
2020-10-16 02:23:14 +0530

    Custom Exception

---
+++ HackerRank Practice/custom_exception.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+
+#define fast ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL)
+#define f(i,a,b) for(auto i=a;i<b;i++)
+#define fi(i,a,b,x) for(auto i=a;i<b;i=i+x)
+#define fe(i,a,b) for(auto i=a;i<=b;i++)
+#define fr(i,a,b) for(auto i=a;i>=b;i--)
+#define loop(i, a) for(auto i=a.begin();i!=a.end();i++)
+#define endl '\n'
+#define sp '\t'
+#define ll long long int
+#define ff first
+#define ss second
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define mod 1000000007
+#define PI 3.1415926535897932384626
+#define deb(x) cout<<#x<<"="<<x<<endl
+#define deb2(x, y) cout<<#x<<" = "<<x<<","<<#y<<" = "<<y<<endl
+
+using namespace std;
+
+template <typename T>
+void print(vector<T>& a, char sep)
+{
+    for(auto i : a) { cout<<i<<sep; }
+}
+
+ll mod_opr(ll num)
+{
+    return (num + mod) % mod;
+}
+
+template <typename T>
+bool compare(T x, T y)
+{
+    return x > y ? true : false;
+}
+
+// Custom Exception
+struct BadLengthException : exception 
+{
+  string s;
+  BadLengthException(int n) : s(to_string(n)) {}
+  const char *what() const noexcept override 
+  {
+    return s.c_str();
+  }
+};
+
+// Function to Check Username
+bool checkUsername(string username) {
+	bool isValid = true;
+	int n = username.length();
+	if(n < 5) {
+		throw BadLengthException(n);
+	}
+	for(int i = 0; i < n-1; i++) {
+		if(username[i] == 'w' && username[i+1] == 'w') {
+			isValid = false;
+		}
+	}
+	return isValid;
+}
+
+
+int main()
+{
+    fast;
+    int T; 
+    cin >> T;
+	while(T--) {
+		string username;
+		cin >> username;
+		try {
+			bool isValid = checkUsername(username);
+			if(isValid) {
+				cout << "Valid" << '\n';
+			} else {
+				cout << "Invalid" << '\n';
+			}
+		} catch (BadLengthException e) {
+			cout << "Too short: " << e.what() << '\n';
+		}
+	}
+	return 0;
+}
